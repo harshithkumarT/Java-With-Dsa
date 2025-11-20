bfeaae61451125441ec7c47be914c8e4185629e8
harshithkumar T <harshithkumar3032005@gmail.com>
2025-11-20 21:36:20 +0530

    commited

--- Matrix_Datastructure/SearchA2DMatrix.java
+++ Matrix_Datastructure/SearchA2DMatrix.java
@@ -29,5 +29,26 @@ public class SearchA2DMatrix {
                 };
         int target = 3;
         System.out.println(searchMatrix(matrix,target));
+        System.out.println(searchMatrixUsingBinarySearch(matrix,target));
+    }
+    public static boolean searchMatrixUsingBinarySearch(int[][] matrix, int target) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int low = 0;
+        int high  = row * col -1;
+        while(low <= high) {
+            int mid = (low+high) / 2;
+            int r = mid/col;
+            int c = mid% col;
+            if(matrix[r][c] == target) {
+                return true;
+            }else if (matrix[r][c] > target) {
+                high = mid-1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return false;
     }
 }
